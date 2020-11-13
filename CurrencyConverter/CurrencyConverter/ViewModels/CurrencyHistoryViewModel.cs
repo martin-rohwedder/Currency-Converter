04364fba823f790c099b5be1a67f8269309edac8
Martin Rohwedder <m.rohwedder@gmail.com>
2020-11-13 13:00:40 +0100

    Currency Histopry Page Added. No content

---
+++ CurrencyConverter/CurrencyConverter/ViewModels/CurrencyHistoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyConverter.ViewModels
+{
+    public class CurrencyHistoryViewModel : BaseViewModel
+    {
+    }
+}
